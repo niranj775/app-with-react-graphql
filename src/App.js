f33995d1253e6a4a529a4d09fd9950408695b921
niranj775 <niranj775@gmail.com>
2020-08-01 19:41:02 +0530

    40%

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { PokemonsContainer } from './containers/PokemonsContainer';
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>App Component</h1>
-    </div>
-  );
+export default function App() {
+    const client = new ApolloClient({
+        uri: 'https://graphql-pokemon.now.sh'
+    });
+    
+    return (
+        <ApolloProvider client={client}>
+            <main>
+                <PokemonsContainer />
+            </main>
+        </ApolloProvider>
+    )
 }
-
-export default App;
